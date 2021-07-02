bf8e6710605a5da8716401e9ce549bff5c3f2fe1
babsmbaye18 <40393271+babsmbaye18@users.noreply.github.com>
2021-07-02 10:49:58 +0200

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -23,7 +23,7 @@ public class TestGreeter {
      String someone1 = "TOTO";
 
     //assertThat(greeter.greet(someone), containsString(someone));
-     assertThat(greeter.greet(someone), someone1);
+     assertThat(greeter.greet(someone), is(someone1));
   }
 
   @Test
